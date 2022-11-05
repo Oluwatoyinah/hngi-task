a72fbff6fb0bba6775326f82f5cdecbcbd68eeb3
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2022-11-05 18:27:39 +0000

    fix: checkbox validation added

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,7 +3,7 @@ import * as yup from "yup";
 export const basicSchema = yup.object().shape({
   first_name: yup.string(),
   last_name: yup.string(),
-  email: yup.string().email("Please enter a valid email"),
+  email: yup.string().email("Please enter a valid email").required('Please provide an email'),
   message: yup.string().required("Please enter a message"),
-  terms_and_condition: yup.boolean(),
+  terms_and_condition: yup.boolean().oneOf([true], "You must agree to the condition"),
 });
